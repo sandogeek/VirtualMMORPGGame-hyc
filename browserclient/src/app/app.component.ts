5f4fb918783f892dcd626b927e1a48f55e095c45
sandogeek <sandogeek@163.com>
2018-10-11 21:57:31 +0800

    调试与websocket服务器的通讯

--- browserclient/src/app/app.component.ts
+++ browserclient/src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit, AfterViewInit } from '@angular/core';
+import { WebSocketSubject } from 'rxjs/observable/dom/WebSocketSubject';
 
+export class Message {
+  constructor(
+    public packetID: number,
+    public content: string,
+  ) { }
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private wsocket: WebSocketSubject<Message>;
+  private wsocket2: WebSocketSubject<Message>;
+  clientMessage = '';
   title = 'browser-client';
+
+  constructor() {
+    this.wsocket = new WebSocketSubject('ws://localhost:8088');
+    this.wsocket2 = new WebSocketSubject('ws://localhost:8083/websocket/?request=e2lkOjE7cmlkOjI2O3Rva2VuOiI0MzYwNjgxMWM3MzA1Y2NjNmFiYjJiZTExNjU3OWJmZCJ9');
+
+    this.wsocket
+      .subscribe(
+        (message) => console.log(message),
+        (err) => console.error(err),
+        () => console.warn('Completed!')
+      );
+    this.wsocket2
+      .subscribe(
+        (message) => console.log(message),
+        (err) => console.error(err),
+        () => console.warn('Completed!')
+      );
+  }
+  public send(): void {
+    const message = new Message(10001, this.clientMessage);
+    this.wsocket.next(message);
+    this.wsocket2.next(message);
+    this.clientMessage = '';
+  }
 }
