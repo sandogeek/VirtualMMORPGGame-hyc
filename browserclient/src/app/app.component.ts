81462edf62abb98504977a94cb42ad3ace90b442
sandogeek <sandogeek@163.com>
2018-10-12 18:23:26 +0800

    测试destination用途

--- browserclient/src/app/app.component.ts
+++ browserclient/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, ElementRef, OnInit, AfterViewInit } from '@angular/core';
 import { WebSocketSubject } from 'rxjs/observable/dom/WebSocketSubject';
+import { Observable, Observer } from 'rxjs';
 
 export class Message {
   constructor(
@@ -13,19 +14,28 @@ export class Message {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  webSocket: WebSocket;
   private wsocket: WebSocketSubject<Message>;
   clientMessage = '';
   title = 'browser-client';
 
   constructor() {
-    this.wsocket = new WebSocketSubject('ws://localhost:8088/websocket');
+    let destination: Observer<Message> = {
+      next: (value: Message) => console.log(value),
+      error: (err: any) => console.error(err),
+      complete: () => console.warn('Completed!')
+    };
+    this.wsocket = new WebSocketSubject('ws://localhost:8088/',destination);
+    // this.wsocket = new WebSocketSubject('ws://localhost:8088/');
 
-    this.wsocket
-      .subscribe(
-        (message) => console.log(message),
-        (err) => console.error(err),
+    this.wsocket.subscribe(
+        (message) => console.log("subscribe:"+JSON.stringify(message,null,4)),
+        (err) => {
+          console.log("来自subscribe");
+          console.error(err);
+        },
         () => console.warn('Completed!')
-      );
+    );
   }
   public send(): void {
     const message = new Message(10001, this.clientMessage);
