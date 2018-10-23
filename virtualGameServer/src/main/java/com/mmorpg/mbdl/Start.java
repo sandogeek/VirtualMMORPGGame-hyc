f4b8348eb5136714aad107fd35d4cdcfde7bc218
sandogeek <sandogeek@163.com>
2018-10-23 18:06:57 +0800

    自动生成PacketId.ts和.proto

--- virtualGameServer/src/main/java/com/mmorpg/mbdl/Start.java
+++ virtualGameServer/src/main/java/com/mmorpg/mbdl/Start.java
@@ -1,7 +1,10 @@
 package com.mmorpg.mbdl;
 
 import com.mmorpg.mbdl.framework.communicate.websocket.model.AbstractPacket;
+import com.mmorpg.mbdl.framework.communicate.websocket.model.PacketIdTsGenerator;
+import com.mmorpg.mbdl.framework.communicate.websocket.model.ProtoGenerator;
 import com.mmorpg.mbdl.framework.communicate.websocket.server.WebSocketServer;
+import com.mmorpg.mbdl.framework.utils.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
@@ -11,8 +14,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Start {
     private static Logger logger= LoggerFactory.getLogger(Start.class);
     public static void  main(String[] args) throws Exception {
+        clearProto(ProtoGenerator.PROTO_PATH);
         // 启动spring容器
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        PacketIdTsGenerator.generatePacketIdTs();
         removeAbstractPacketBean(ctx);
         logger.info("开始启动WebSocket服务器...");
         WebSocketServer webSocketServer = WebSocketServer.getInstance();
@@ -29,5 +34,12 @@ public class Start {
             BeanDefinitionRegistry beanDefReg = (DefaultListableBeanFactory)ctx.getBeanFactory();
             beanDefReg.removeBeanDefinition(beanName);
         }
+    }
+    /**
+     * 清空dir下的.proto文件
+     * @param dir
+     */
+    private static void clearProto(String dir) {
+        FileUtils.clearByFileFilter(dir,true,FileUtils.extFileFilter("proto"));
     }
 }
