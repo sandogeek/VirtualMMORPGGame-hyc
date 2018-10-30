ce51af685a5ed64102466c25b512477c1d7f7cae
sandogeek <sandogeek@163.com>
2018-10-30 15:52:12 +0800

    test:session状态校验。

--- virtualGameServer/src/main/java/com/mmorpg/mbdl/bussiness/login/facade/LoginFacade.java
+++ virtualGameServer/src/main/java/com/mmorpg/mbdl/bussiness/login/facade/LoginFacade.java
@@ -13,7 +13,7 @@ import org.slf4j.LoggerFactory;
 @PacketHandler
 public class LoginFacade {
     private static final Logger logger = LoggerFactory.getLogger(LoginFacade.class);
-    // @PacketMethod(state = SessionState.CONNECTED)
+    @PacketMethod(state = SessionState.CONNECTED)
     public LoginResultResp loginAuth(WsSession session, LoginAuthReq req){
         // TODO 密码采用非对称加密传输并存储到数据库
         // String message = String.format("协议[%s-%s]分发成功：帐号：%s,密码：%s",req.getPacketId(),req.getClass().getSimpleName(),req.getAccount(),req.getPassword());
