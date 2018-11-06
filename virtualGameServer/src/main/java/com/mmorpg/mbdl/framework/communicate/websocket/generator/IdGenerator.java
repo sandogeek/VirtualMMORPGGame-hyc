b67536fb70a8de7719a5797ae666c43f7b197abd
sandogeek <sandogeek@163.com>
2018-11-06 11:53:16 +0800

    feat:雪花算法IdGenerator优化，生成的Id hash更均匀，最多支持16条线程无锁竞争地生成Id，在i5 7200u 2.5GHz上测试发现每秒大约能生成200w个分布式Id

--- virtualGameServer/src/main/java/com/mmorpg/mbdl/framework/communicate/websocket/generator/IdGenerator.java
+++ virtualGameServer/src/main/java/com/mmorpg/mbdl/framework/communicate/websocket/generator/IdGenerator.java
@@ -2,18 +2,11 @@ package com.mmorpg.mbdl.framework.communicate.websocket.generator;
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import io.netty.util.internal.ConcurrentSet;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.StopWatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import java.text.DateFormat;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.Set;
 import java.util.concurrent.*;
 
@@ -25,27 +18,26 @@ import java.util.concurrent.*;
  * 得到的值，这里的的开始时间截，一般是我们的id生成器开始使用的时间，由我们程序来指定的（如下下面程序PlayerIdGenerator类的startTime属性）。
  * 39位的时间截，大约可以使用17年，年T = (1L << 39) / (1000L * 60 * 60 * 24 * 365)<br>
  * 11位的数据机器位，可以部署在2048个节点，包括5位datacenterId和6位serverId<br>
- * 9位序列，毫秒内的计数，7位的计数顺序号支持每个节点每毫秒(同一机器，同一时间截)产生128个ID序号,每秒大约能生成10w个id<br>
- * 只要每秒注册人数不超过10w就不会产生问题<br>
+ * 9位序列，毫秒内的计数，9位的计数顺序号支持每个节点每毫秒(同一机器，同一时间截)产生512个ID序号,每秒大约能生成10w个id<br>
+ * 只要每秒注册人数不超过51w就不会产生问题<br>
  * 加起来刚好64位，为一个Long型。<br>
  * SnowFlake的优点是，整体上按照时间自增排序，并且整个分布式系统内不会产生ID碰撞(由数据中心ID和机器ID作区分)，并且效率较高，经测试，SnowFlake每秒能够产生26万ID左右。
  */
-@Component
-public class PlayerIdGenerator implements IIdGenerator {
-    private static final Logger logger = LoggerFactory.getLogger(PlayerIdGenerator.class);
+class IdGenerator implements IIdGenerator {
+    private static final Logger logger = LoggerFactory.getLogger(IdGenerator.class);
 
-    /** 线程id所占的位数,目前留空 */
-    private static final long threadIdBits = 4L;
-    /** 最大线程id量结果是64,因为用来求余，所以加1 */
-    private static final long maxThreadId = ~(-1L << threadIdBits);
+    /** Generator id所占的位数 */
+    private static final long generatorIdBits = 4L;
+    /** 最大Generator id量结果是15 */
+    private static final long maxGeneratorId = ~(-1L << generatorIdBits);
     /** 数据标识id所占的位数 */
     private static final long datacenterIdBits = 5L;
     /** 支持的最大数据标识id，结果是31 */
-    private static final long maxDatacenterId = ~(-1L << datacenterIdBits);
+    static final long maxDatacenterId = ~(-1L << datacenterIdBits);
     /** 机器id所占的位数 */
     private static final long serverIdBits = 6L;
     /** 支持的最大机器id，结果是63 (这个移位算法可以很快的计算出几位二进制数所能表示的最大十进制数) */
-    private static final long maxServerId = ~(-1L << serverIdBits);
+    static final long maxServerId = ~(-1L << serverIdBits);
 
     /** 序列在id中占的位数 */
     private static final long sequenceBits = 9L;
@@ -55,42 +47,31 @@ public class PlayerIdGenerator implements IIdGenerator {
     private static final long datacenterIdShift= serverIdShift + serverIdBits;
     /** 线程Id左移5+15=20位 */
     private static final long threadIdShift = datacenterIdShift + datacenterIdBits;
-    /** 时间截向左移24位(5+15) + 4 4位预留位，用于存放其他信息*/
-    private static final long timestampLeftShift = threadIdShift + threadIdBits;
+    /** 时间截向左移24位(5+15) + 4*/
+    private static final long timestampLeftShift = threadIdShift + generatorIdBits;
     /** 生成序列的掩码，512-1 */
     private static final long sequenceMask = ~(-1L << sequenceBits);
 
-    /** 开始时间截 */
-    @Value("${server.config.beginOn}")
-    private String beginOnString = "2018/10/30";
-    private long beginOn ;
-    /** 数据中心ID(0~31) */
-    @Value("${server.config.dataCenterId}")
-    private long datacenterId = 1;
-    /** 服务器ID(0~63) */
-    @Value("${server.config.serverId}")
-    private long serverId = 1;
 
-    /** 毫秒内序列号(0~255) */
+
+    /** 毫秒内序列号(0~511) */
     private long sequence = 0L;
+    /** 毫秒内id生成计数器,由于大多数情况下生成的id不在同一毫秒，按原方式容易导致很多Id末尾是一串0，这里用计数器使Id末尾数字更均匀 */
+    private long count = 0L;
 
     /** 上次生成ID的时间截 */
     private long lastTimestamp = -1L;
 
-    @PostConstruct
-    private void init(){
-        String[] date = StringUtils.split(this.beginOnString,'/');
-        Calendar calendar = new Calendar.Builder().setDate(Integer.valueOf(date[0]),Integer.valueOf(date[1])-1,Integer.valueOf(date[2])).build();
-        this.beginOn = calendar.getTimeInMillis();
-        if (serverId > maxServerId || serverId < 0) {
-            throw new IllegalArgumentException(String.format("服务器Id不能大于%d或小于0", maxServerId));
-        }
-        if (datacenterId > maxDatacenterId || datacenterId < 0) {
-            throw new IllegalArgumentException(String.format("数据中心Id不能大于%d或小于0", maxDatacenterId));
-        }
-        if (this.beginOn >= System.currentTimeMillis()) {
-            throw new IllegalArgumentException("当前时间未到设定的运营时间: " + DateFormat.getDateInstance().format(new Date(this.beginOn)) + "无法生成id");
-        }
+    private long generatorId;
+    private long datacenterId;
+    private long serverId;
+    private long beginOn ;
+
+    public IdGenerator(long generatorId, long datacenterId, long serverId, long beginOn) {
+        this.generatorId = generatorId;
+        this.datacenterId = datacenterId;
+        this.serverId = serverId;
+        this.beginOn = beginOn;
     }
 
     /**
@@ -122,20 +103,23 @@ public class PlayerIdGenerator implements IIdGenerator {
 
         //如果上次生成时间和当前时间相同,在同一毫秒内
         if (lastTimestamp == timestamp) {
-            //sequence自增，因为sequence只有7bit，所以和sequenceMask相与一下，去掉高位
-            sequence = (sequence+1) & sequenceMask;
-            //判断是否溢出,也就是每毫秒内超过512，当为512时，与sequenceMask相与，sequence就等于0
-            if (sequence == 0) {
+            //count自增，因为sequence只有9bit，所以和sequenceMask相与一下，去掉高位
+            count = (count+1) & sequenceMask;
+            sequence = (sequence + 1) & sequenceMask;
+            //判断是否溢出,也就是每毫秒内超过512，当为512时，与sequenceMask相与，count就等于0
+            if (count == 0) {
                 //自旋等待到下一毫秒
                 timestamp = tailNextMillis(lastTimestamp);
             }
         } else {
-            // 如果和上次生成时间不同,重置sequence，就是下一毫秒开始，sequence计数重新从0开始累加
-            sequence=0;
+            // 如果和上次生成时间不同,重置count，就是下一毫秒开始，count计数重新从0开始累加
+            count = 0;
+            sequence = (sequence + 1) & sequenceMask;
         }
+        // logger.info("{}",sequence);
         lastTimestamp = timestamp;
         return ((timestamp - this.beginOn) << timestampLeftShift)
-                // | (Thread.currentThread().getChatChannelId()%16 << threadIdShift)
+                | ( generatorId << threadIdShift)
                 | (datacenterId << datacenterIdShift)
                 | (serverId << serverIdShift)
                 | sequence;
@@ -158,38 +142,41 @@ public class PlayerIdGenerator implements IIdGenerator {
     /** 测试 */
     public static void main(String[] args) {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-        PlayerIdGenerator playerIdGenerator = ctx.getBean(PlayerIdGenerator.class);
+        IdGeneratorFactory idGeneratorFactory = ctx.getBean(IdGeneratorFactory.class);
+        IdGenerator idGenerator0 = idGeneratorFactory.getPlayerIdGenerator();
         Set<Long> longsConcurrent = new ConcurrentSet<>();
         ThreadFactory namedThreadFactory = new ThreadFactoryBuilder()
                 .setNameFormat("test-pool-%d").build();
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
-        for (int j = 0; j < 30_0000; j++) {
-            playerIdGenerator.generate();
+        for (int j = 0; j < 200_0000; j++) {
+            idGenerator0.generate();
+            // logger.info("{}",idGeneratorFactory.getObjectIdGenerator().generatorId);
         }
         stopWatch.stop();
         logger.info("耗时{}ms",stopWatch.getTime());
 
-        int threadSize = 6;
+        int threadSize = 10;
         ExecutorService threadPool = new ThreadPoolExecutor(threadSize, threadSize,
                 0L, TimeUnit.MILLISECONDS,
                 new LinkedBlockingQueue<Runnable>(1024), namedThreadFactory, new ThreadPoolExecutor.AbortPolicy());
         for (int i = 0; i < threadSize; i++) {
             threadPool.execute(()->{
                 // logger.info("{}",Thread.currentThread().getChatChannelId()%16);
+                // logger.info("{}",idGeneratorFactory.getObjectIdGenerator().generatorId);
+                IdGenerator idGenerator = idGeneratorFactory.getObjectIdGenerator();
                 long start = System.currentTimeMillis();
-                for (int j = 0; j < 30_0000/threadSize; j++) {
-                    long id = playerIdGenerator.generate();
-                    // logger.info("{}",id);
-                    if (longsConcurrent.contains(id)){
-                        logger.info("{}",Long.toBinaryString(id).substring(0,Math.toIntExact(64-timestampLeftShift)));
-                    }else {
-                        longsConcurrent.add(id);
-                    }
+                for (int j = 0; j < 200_0000/threadSize; j++) {
+                    long id = idGenerator.generate();
+                    // if (longsConcurrent.contains(id)){
+                    //     logger.info("{}",Long.toBinaryString(id).substring(0,Math.toIntExact(64-timestampLeftShift)));
+                    // }else {
+                    //     longsConcurrent.add(id);
+                    // }
                 }
                 // stopwatch.stop();
                 // 注意，因为是并发，所以时长以最后完成的时间为准
-                logger.info(Thread.currentThread().getName()+"完成,"+"生成{}个id耗时：{}ms",300000,System.currentTimeMillis()- start);
+                logger.info(Thread.currentThread().getName()+"完成,"+"耗时：{}ms",System.currentTimeMillis()- start);
             });
         }
         threadPool.shutdown();
---
+++ virtualGameServer/src/main/java/com/mmorpg/mbdl/framework/communicate/websocket/generator/IdGeneratorFactory.java
@@ -0,0 +1,88 @@
+package com.mmorpg.mbdl.framework.communicate.websocket.generator;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Id生成器工厂
+ * @author sando
+ */
+@Component
+public class IdGeneratorFactory {
+    private static final Logger logger = LoggerFactory.getLogger(IdGeneratorFactory.class);
+    private static final long maxServerId = IdGenerator.maxServerId;
+    private static final long maxDatacenterId = IdGenerator.maxDatacenterId;
+    /** 开始时间截 TODO 以下字段变为静态字段并使用@Prop直接注入 */
+    @Value("${server.config.beginOn}")
+    private String beginOnString = "2018/10/30";
+    private long beginOn ;
+    /** 数据中心ID(0~31) */
+    @Value("${server.config.dataCenterId}")
+    private long datacenterId = 1;
+    /** 服务器ID(0~63) */
+    @Value("${server.config.serverId}")
+    private long serverId = 1;
+    /** 用来生成玩家Id的实例的数量 */
+    private int maxPlayerInstance = 6;
+    /** 玩家IdGenerator实例数组下标 */
+    private AtomicInteger arrayIndexOfPlayer = new AtomicInteger(-1);
+    /** 用来生成对象Id的实例的数量 */
+    private int maxObjectInstance = 10;
+    /** 对象IdGenerator实例数组下标 */
+    private AtomicInteger arrayIndexOfObject = new AtomicInteger(-1);
+    private IdGenerator[] playerInstanceIdGenerators = new IdGenerator[maxPlayerInstance];
+    private IdGenerator[] ObjectInstanceIdGenerators = new IdGenerator[maxObjectInstance];
+
+    private static IdGeneratorFactory self;
+    public static IdGeneratorFactory getIntance(){
+        return self;
+    }
+    @PostConstruct
+    private void init(){
+        self = this;
+        String[] date = StringUtils.split(this.beginOnString,'/');
+        Calendar calendar = new Calendar.Builder().setDate(Integer.valueOf(date[0]),Integer.valueOf(date[1])-1,Integer.valueOf(date[2])).build();
+        this.beginOn = calendar.getTimeInMillis();
+        if (serverId > maxServerId || serverId < 0) {
+            throw new IllegalArgumentException(String.format("服务器Id不能大于%d或小于0", maxServerId));
+        }
+        if (datacenterId > maxDatacenterId || datacenterId < 0) {
+            throw new IllegalArgumentException(String.format("数据中心Id不能大于%d或小于0", maxDatacenterId));
+        }
+        if (this.beginOn >= System.currentTimeMillis()) {
+            throw new IllegalArgumentException("当前时间未到设定的运营时间: " + DateFormat.getDateInstance().format(new Date(this.beginOn)) + "无法生成id");
+        }
+        for (int i = 0; i < playerInstanceIdGenerators.length; i++) {
+            playerInstanceIdGenerators[i] = new IdGenerator(i,datacenterId,serverId,beginOn);
+        }
+        for (int i = 0; i < ObjectInstanceIdGenerators.length; i++) {
+            // +6保证生成器id不同
+            ObjectInstanceIdGenerators[i] = new IdGenerator(i+6,datacenterId,serverId,beginOn);
+        }
+    }
+
+    /**
+     * 获取玩家IdGenerator
+     * @return IdGenerator
+     */
+    public IdGenerator getPlayerIdGenerator(){
+        return playerInstanceIdGenerators[arrayIndexOfPlayer.updateAndGet(n -> (n+1)%maxPlayerInstance)];
+    }
+
+    /**
+     * 获取对象IdGenerator
+     * @return IdGenerator
+     */
+    public IdGenerator getObjectIdGenerator(){
+        return ObjectInstanceIdGenerators[arrayIndexOfObject.updateAndGet(n -> (n+1)%maxObjectInstance)];
+    }
+}
