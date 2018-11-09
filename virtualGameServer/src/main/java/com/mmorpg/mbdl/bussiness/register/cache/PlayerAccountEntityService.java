971dba1bd4c7890a199519244ca80057039fa25c
sandogeek <sandogeek@163.com>
2018-11-09 18:32:40 +0800

    缓存测试，发现调用缓存失效功能失败，待修复

--- virtualGameServer/src/main/java/com/mmorpg/mbdl/bussiness/register/cache/PlayerAccountEntityService.java
+++ virtualGameServer/src/main/java/com/mmorpg/mbdl/bussiness/register/cache/PlayerAccountEntityService.java
@@ -1,9 +1,11 @@
 package com.mmorpg.mbdl.bussiness.register.cache;
 
 import com.github.xiaolyuh.annotation.*;
+import com.mmorpg.mbdl.bussiness.register.dao.PlayerAccountEntityDAO;
 import com.mmorpg.mbdl.bussiness.register.entity.PlayerAccountEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,32 +15,29 @@ import org.springframework.stereotype.Component;
 public class PlayerAccountEntityService {
     Logger logger = LoggerFactory.getLogger(PlayerAccountEntityService.class);
 
+    @Autowired
+    private PlayerAccountEntityDAO playerAccountEntityDAO;
+
     @CachePut(value = "player.account", key = "#playerAccount.playerId", depict = "用户信息缓存")
-    public PlayerAccountEntity save(PlayerAccountEntity playerAccount) {
+    public PlayerAccountEntity saveAndFlush(PlayerAccountEntity playerAccount) {
         logger.info("为id、key为:" + playerAccount.getPlayerId() + "数据做了缓存");
-        return playerAccount;
+        return playerAccountEntityDAO.saveAndFlush(playerAccount);
     }
 
-    @CacheEvict(value = "player.account", key = "#id")//2
-    public void remove(Long id) {
+    @CacheEvict(value = "player.account", key = "#id")
+    public void delete(Long id) {
         logger.info("删除了id、key为" + id + "的数据缓存");
-        //这里不做实际删除操作
+        playerAccountEntityDAO.delete(id);
     }
 
-    @CacheEvict(value = "player.account", allEntries = true)//2
-    public void removeAll() {
+    @CacheEvict(value = "player.account", allEntries = true)
+    public void invalidateAll() {
         logger.info("删除了所有缓存的数据缓存");
-        //这里不做实际删除操作
     }
 
-    @Cacheable(value = "player.account", key = "#playerAccount.playerId", depict = "用户信息缓存")
-    public PlayerAccountEntity findOne(PlayerAccountEntity playerAccount) {
-        PlayerAccountEntity playerAccountEntity = new PlayerAccountEntity();
-        playerAccountEntity.setAccount("sando1");
-        playerAccountEntity.setPassword("123556");
-        // IdGenerator idGenerator = IdGeneratorFactory.getIntance().getPlayerIdGenerator();
-        playerAccountEntity.setPlayerId(3L);
-        logger.info("为id、key为:" + playerAccountEntity.getPlayerId() + "数据做了缓存");
-        return playerAccountEntity;
+    @Cacheable(value = "player.account", key = "#id", depict = "用户信息缓存")
+    public PlayerAccountEntity get(Long id) {
+        logger.info("为id、key为:" + id + "数据做了缓存");
+        return playerAccountEntityDAO.findOne(id);
     }
 }
--- virtualGameServer/src/test/java/com/mmorpg/mbdl/bussiness/register/cache/PlayerAccountEntityServiceTest.java
+++ virtualGameServer/src/test/java/com/mmorpg/mbdl/bussiness/register/cache/PlayerAccountEntityServiceTest.java
@@ -4,38 +4,67 @@ import com.mmorpg.mbdl.bussiness.register.entity.PlayerAccountEntity;
 import com.mmorpg.mbdl.framework.common.generator.IdGenerator;
 import com.mmorpg.mbdl.framework.common.generator.IdGeneratorFactory;
 import junit.framework.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Random;
+
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration("classpath:applicationContext.xml")
 class PlayerAccountEntityServiceTest {
+    private static final Logger logger = LoggerFactory.getLogger(PlayerAccountEntityServiceTest.class);
     @Autowired
     PlayerAccountEntityService playerAccountEntityService;
 
     @Test
     void save() {
+        long playerId = 10000L;
+        long playerId2 = 10001L;
         PlayerAccountEntity playerAccountEntity = new PlayerAccountEntity();
-        playerAccountEntity.setAccount("sando1");
+        playerAccountEntity.setAccount("sando"+ new Random().nextInt(300));
         playerAccountEntity.setPassword("123556");
         IdGenerator idGenerator = IdGeneratorFactory.getIntance().getPlayerIdGenerator();
-        long playerId = idGenerator.generate();
         playerAccountEntity.setPlayerId(playerId);
+        for (int i=0;i<5;i++){
+            try {
+                playerAccountEntityService.saveAndFlush(playerAccountEntity);
+                // playerAccountEntityService.saveAndFlush(playerAccountEntity2);
+                break;
+            }catch (Exception e){
+            }
+        }
 
-        playerAccountEntityService.save(playerAccountEntity);
-        PlayerAccountEntity playerAccountEntity1 = playerAccountEntityService.findOne(playerAccountEntity);
+        PlayerAccountEntity playerAccountEntity1 = playerAccountEntityService.get(playerId);
         Assert.assertEquals(new Long(playerId), playerAccountEntity1.getPlayerId());
     }
 
     @Test
-    void remove() {
+    void delete() throws Exception {
+        long playerId = 10001L;
+        PlayerAccountEntity playerAccountEntity2 = new PlayerAccountEntity();
+        playerAccountEntity2.setAccount("sando"+ new Random().nextInt(300));
+        playerAccountEntity2.setPassword("123556");
+        // IdGenerator idGenerator = IdGeneratorFactory.getIntance().getPlayerIdGenerator();
+        playerAccountEntity2.setPlayerId(playerId);
+        playerAccountEntityService.saveAndFlush(playerAccountEntity2);
+        Thread.sleep(5000);
+        playerAccountEntityService.delete(playerId);
+        // Assertions.assertThrows(EmptyResultDataAccessException.class,()->{
+        //     PlayerAccountEntity accountEntity = playerAccountEntityService.get(playerId);
+        //     logger.info("{}",accountEntity);
+        // });
+        Assertions.assertNull(playerAccountEntityService.get(playerId));
     }
 
     @Test
-    void removeAll() {
+    void invalidatAll() {
+        playerAccountEntityService.invalidateAll();
     }
 
     @Test
