26eec90ad75dc78991b94cf583b0b82fb2aca13a
sandogeek <sandogeek@163.com>
2019-02-02 17:05:13 +0800

    装备

---
+++ virtualGameServer/src/test/java/com/mmorpg/mbdl/business/container/resource/ItemRes11.java
@@ -0,0 +1,78 @@
+package com.mmorpg.mbdl.business.container.resource;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.mmorpg.mbdl.framework.resource.annotation.Key;
+import com.mmorpg.mbdl.framework.resource.annotation.ResDef;
+
+/**
+ * 物品资源
+ *
+ * @author Sando Geek
+ * @since v1.0
+ **/
+@ResDef
+public class ItemRes11 {
+    @Key
+    @JsonProperty(value = "Id")
+    private String id;
+
+    @JsonProperty(value = "Name")
+    private String name;
+
+    @JsonProperty(value = "Quality")
+    private int quality;
+
+    @JsonProperty(value = "CanUse")
+    private boolean canUse;
+
+    @JsonProperty(value = "Type")
+    private int type;
+
+    @JsonProperty(value = "Func")
+    private int func;
+
+    @JsonProperty(value = "Parameter1")
+    private int parameter1;
+
+    @JsonProperty(value = "Parameter2")
+    private int parameter2;
+
+    @JsonProperty(value = "Parameter3")
+    private int parameter3;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public boolean isCanUse() {
+        return canUse;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public int getFunc() {
+        return func;
+    }
+
+    public int getParameter1() {
+        return parameter1;
+    }
+
+    public int getParameter2() {
+        return parameter2;
+    }
+
+    public int getParameter3() {
+        return parameter3;
+    }
+}
