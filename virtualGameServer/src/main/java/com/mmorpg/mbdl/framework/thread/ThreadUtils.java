5478a61c61a973f9d98446a23251993191dda275
sandogeek <sandogeek@163.com>
2019-11-14 01:40:19 +0800

    feat(线程)：改用ThreadLocal存储当前线程对应的任务

--- virtualGameServer/src/main/java/com/mmorpg/mbdl/framework/thread/ThreadUtils.java
+++ virtualGameServer/src/main/java/com/mmorpg/mbdl/framework/thread/ThreadUtils.java
@@ -4,8 +4,6 @@ import com.mmorpg.mbdl.framework.thread.interfaces.Dispatchable;
 import com.mmorpg.mbdl.framework.thread.task.AbstractTask;
 
 import java.io.Serializable;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * 线程工具类
@@ -17,7 +15,7 @@ public class ThreadUtils {
     /**
      * 当前线程正在执行哪个Task
      */
-    private static Map<Thread, AbstractTask<? extends Dispatchable<? extends Serializable>>> thread2AbstractTask = new ConcurrentHashMap<>();
+    private static ThreadLocal<AbstractTask<? extends Dispatchable<? extends Serializable>>> taskThreadLocal = new ThreadLocal<>();
 
     /**
      * 获取当前线程正在执行的任务的可分发对象
@@ -34,7 +32,7 @@ public class ThreadUtils {
      * @return
      */
     public static AbstractTask<? extends Dispatchable<? extends Serializable>> currentThreadTask() {
-        AbstractTask<? extends Dispatchable<? extends Serializable>> abstractTask = thread2AbstractTask.get(Thread.currentThread());
+        AbstractTask<? extends Dispatchable<? extends Serializable>> abstractTask = taskThreadLocal.get();
         if (abstractTask == null) {
             throw new RuntimeException("此方法必须在任务中AbstractTask任务内运行的代码中调用");
         }
@@ -47,14 +45,14 @@ public class ThreadUtils {
      * @param abstractTask
      */
     public static void setCurrentThreadTask(AbstractTask<? extends Dispatchable<? extends Serializable>> abstractTask) {
-        thread2AbstractTask.put(Thread.currentThread(), abstractTask);
+        taskThreadLocal.set(abstractTask);
     }
 
     /**
      * 移除当前线程对应的{@link AbstractTask},任务结束时调用
      */
     public static void removeCurrentThreadTask() {
-        thread2AbstractTask.remove(Thread.currentThread());
+        taskThreadLocal.remove();
     }
 
     private ThreadUtils() {
